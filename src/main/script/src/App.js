d4fb95c4bc32b1954c195c37ccb701000f295aec
sung0513 <rlatjddls367@naver.com>
2020-02-04 13:35:20 +0900

    초기

--- src/main/script/src/App.js
+++ src/main/script/src/App.js
@@ -3,20 +3,14 @@ import 'whatwg-fetch';
 import logo from './logo.svg';
 import './App.css';
 
-function App() extends Component {
-  render() {
+function App() 
   return (
       <div className="App">
         <img src={logo} className="App-logo" alt="logo"/>
         <h2>스프링부트 리액트 연동 확인</h2>
       </div>
-        <form className="App-intro" onSubmit={this.handleSubmit}>
-          <input type="test" onChange={this.handleChange} />
-          <input type="submit" value="pass" />
-        </form>
-    </div>
   );
-}
-}
+
+
 
 export default App;
