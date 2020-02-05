fcd8c673f3a40a3898bff4087a630fb5f268ff6f
JeongWu <park96100331@gmail.com>
2020-02-05 15:41:47 +0900

    component 생성

---
+++ src/main/script/src/components/Content.js
@@ -0,0 +1,14 @@
+import React,{ Component } from 'react';
+
+
+class Content extends React.Component {
+    render(){
+        return (
+            <div>
+                <h2>Content</h2>
+            </div>
+        );
+    }
+}
+
+export default Content;
