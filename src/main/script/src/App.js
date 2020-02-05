dd799e41fcb501bb3c7541afc2d26a4564eacbea
Hyungjin Joo <hjjooace@naver.com>
2020-02-05 15:55:40 +0900

    App.js 오류 수정(주형진)

--- src/main/script/src/App.js
+++ src/main/script/src/App.js
@@ -1,16 +1,17 @@
-import React,{ Component } from 'react';
+import React, { Component } from 'react';
 import 'whatwg-fetch';
 import logo from './logo.svg';
 import './App.css';
 
-function App() 
+function App() {
   return (
-      <div className="App">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <h2>스프링부트 리액트 연동 확인</h2>
-      </div>
+    <div className="App">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2>스프링부트 리액트 연동 확인</h2>
+    </div>
   );
+}
 
 
 
-export default App;
+  export default App;
