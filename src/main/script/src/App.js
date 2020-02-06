55f9d56a7258427cabe4621857f81677b0a0d69d
JeongWu <park96100331@gmail.com>
2020-02-06 23:37:32 +0900

    불필요한 것 삭제

--- src/main/script/src/App.js
+++ src/main/script/src/App.js
@@ -3,8 +3,6 @@ import Header from './components/Header';
 import Search from './components/Search';
 import Content from './components/Content';
 import 'whatwg-fetch';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render(){
