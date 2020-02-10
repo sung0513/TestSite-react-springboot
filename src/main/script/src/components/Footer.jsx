d39bb169762f16d914cb55b2f0a7bcdc915e5cf0
GiHwan123 <lgh95m@naver.com>
2020-02-10 15:48:00 +0900

    testing

--- src/main/script/src/components/Footer.jsx
+++ src/main/script/src/components/Footer.jsx
@@ -4,10 +4,7 @@ import React,{ Component } from 'react';
 class Footer extends Component {
     render(){
         return (
-            <>
             <h1>Footer</h1>
-            <p>lklklklkl</p>
-            </>
         );
     }
 }
