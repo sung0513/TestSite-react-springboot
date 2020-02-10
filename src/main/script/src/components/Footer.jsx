4d2f9b1940de8238a7acd6e2f5cc5d7a976eddd2
JeongWu <park961003@naver.com>
2020-02-10 15:01:13 +0900

    Testing Footer(박정우)

--- src/main/script/src/components/Footer.jsx
+++ src/main/script/src/components/Footer.jsx
@@ -4,7 +4,10 @@ import React,{ Component } from 'react';
 class Footer extends Component {
     render(){
         return (
+            <>
             <h1>Footer</h1>
+            <p>gsdsfd</p>
+            </>
         );
     }
 }
