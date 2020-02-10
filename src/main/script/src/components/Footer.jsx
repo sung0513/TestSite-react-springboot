658d2db2781e2ffce5f4af7f97bd266a7ddc1ce4
JeongWu <park961003@naver.com>
2020-02-10 15:57:28 +0900

    component 내용 변경

--- src/main/script/src/components/Footer.jsx
+++ src/main/script/src/components/Footer.jsx
@@ -4,10 +4,8 @@ import React,{ Component } from 'react';
 class Footer extends Component {
     render(){
         return (
-            <>
-            <h1>Footer</h1>
-            <p>gsdsfd</p>
-            </>
+            <p>(유)딜리버리히어로 코리아
+            서울시 서초구 서초대로38길 12 마제스타시티 타워2 17층|대표자 : 강신봉|사업자등록번호:211-88-68802 사업자정보확인통신판매업신고:제 2018-서울서초-2635호|개인정보담당자 : privacy@yogiyo.co.kr|제휴문의 : partnership@deliveryhero.co.kr|고객만족센터 : support@yogiyo.co.kr|호스트서비스사업자 : (주)심플렉스인터넷</p>
         );
     }
 }
