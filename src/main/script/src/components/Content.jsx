d41caeae8b085af3fe7c61f29174f4f872f9f394
JeongWu <park961003@naver.com>
2020-02-10 23:23:21 +0900

    change content component

--- src/main/script/src/components/Content.jsx
+++ src/main/script/src/components/Content.jsx
@@ -1,18 +1,30 @@
-import React,{ Component } from 'react';
-import FoodImg from '../img/chicken.png'
-import {Card} from 'react-bootstrap'
+import React, { Component } from "react";
+import FoodImg from "../img/chicken.png";
+import { Card, Row } from "react-bootstrap";
 class Content extends Component {
-    
-    render(){
-        return (
-            <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={FoodImg} />
-    <Card.Body>
-  <Card.Title>Chicken</Card.Title>
-  </Card.Body>
-</Card>
-        );
-    }
+  render() {
+    const categoryList = [
+      "치킨",
+      "피자/양식",
+      "중국집",
+      "한식",
+      "일식",
+      "족발/보쌈",
+      "분식"
+    ];
+    return (
+      <Row>
+        {categoryList.map(list => 
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src={FoodImg} />
+            <Card.Body>
+              <Card.Title>{list}</Card.Title>
+            </Card.Body>
+          </Card>
+        )}
+      </Row>
+    );
+  }
 }
 
 export default Content;
