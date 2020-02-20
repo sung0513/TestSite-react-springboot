ab8d7c9fb279a3bc5e33513a554a2bce6f1cf949
GiHwan123 <lgh95m@naver.com>
2020-02-20 18:15:38 +0900

    commit

--- src/main/script/src/components/mainContent.jsx
+++ src/main/script/src/components/mainContent.jsx
@@ -13,33 +13,6 @@ class MainContent extends Component {
     this.foodStore = foodStore;
   }
   render() {
-<<<<<<< HEAD
-    const categoryList = [
-      { id: 0, name: "전체보기"},
-      { id: 1, name: "치킨" },
-      { id: 2, name: "피자/양식" },
-      { id: 3, name: "중국집" },
-      { id: 4, name: "한식" },
-      { id: 5, name: "일식" },
-      { id: 6, name: "족발/보쌈" },
-      { id: 7, name: "분식" }
-    ];
-    return (
-      <Container >
-      <Row>
-        {categoryList.map(list => (
-          <Card key={list.id} className="card" bg="dark" style={{ width: "20rem" }}>
-             <a href={"category"+list.id}><Card.Img
-              width="300"
-              height="150" src={require("../img/category"+list.id+".png")}
-            /></a>
-            <Card.Body>
-              <Card.Title>{list.name}</Card.Title>
-            </Card.Body>
-          </Card>
-        ))}
-      </Row>
-=======
     const { categoryList, handleSelect } = this.foodStore;
     return (
       <Container>
@@ -67,7 +40,6 @@ class MainContent extends Component {
             </Card>
           ))}
         </Row>
->>>>>>> f01eebc4e158b939a6256db36c1f928f1bde59ab
       </Container>
     );
   }
--- src/main/script/src/components/second/secondContent.jsx
+++ src/main/script/src/components/second/secondContent.jsx
@@ -5,21 +5,12 @@ import Storelist from "./Storelist";
 
 class SecondContent extends Component {
   render() {
-<<<<<<< HEAD
-    return( 
-    <>
-    <Category/>
-    <Row>
-    <Storelist />
-    </Row>
-=======
     return (
       <>
         <Category />
         <Row>
           <Storelist />
         </Row>
->>>>>>> f01eebc4e158b939a6256db36c1f928f1bde59ab
       </>
     );
   }
