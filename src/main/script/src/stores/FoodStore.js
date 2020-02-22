73844283b48dae2b397e08e3a7b9a7f521ed7b62
JeongWu <park961003@naver.com>
2020-02-22 11:57:08 +0900

    change eventkey type

--- src/main/script/src/stores/FoodStore.js
+++ src/main/script/src/stores/FoodStore.js
@@ -24,22 +24,22 @@ export default class FoodStore {
   initialize = () => {
     console.log("initialize");
     this.storeList = [
-      { name: "BBQ", tel: "010-8805-6376", category: "1", address: "성남" },
-      { name: "피자헛", tel: "010-8805-6376", category: "2", address: "서울" },
+      { name: "BBQ", tel: "010-8805-6376", category: 1, address: "성남" },
+      { name: "피자헛", tel: "010-8805-6376", category: 2, address: "서울" },
       {
         name: "몽고반점",
         tel: "010-8805-6376",
-        category: "3",
+        category: 3,
         address: "수원"
       },
-      { name: "비비고", tel: "010-8805-6376", category: "4", address: "용인" },
-      { name: "미소야", tel: "010-8805-6376", category: "5", address: "평택" },
-      { name: "가족", tel: "010-8805-6376", category: "6", address: "인천" },
-      { name: "엽떡", tel: "010-8805-6376", category: "7", address: "안산" },
+      { name: "비비고", tel: "010-8805-6376", category: 4, address: "용인" },
+      { name: "미소야", tel: "010-8805-6376", category: 5, address: "평택" },
+      { name: "가족", tel: "010-8805-6376", category: 6, address: "인천" },
+      { name: "엽떡", tel: "010-8805-6376", category: 7, address: "안산" },
       {
         name: "현우네족발집",
         tel: "010-8805-6376",
-        category: "6",
+        category: 6,
         address: "부산"
       }
     ];
@@ -48,7 +48,7 @@ export default class FoodStore {
   @action
   changeTab = e => {
     console.log("changeTab");
-    this.tabkey = e;
+    this.tabkey = parseInt(e); //eventKey가 string으로 넘어옴
     this.changeList();
   };
 
@@ -59,7 +59,7 @@ export default class FoodStore {
     const list = [...this.storeList];
     const key = this.tabkey;
     this.storeList =
-      key === "0" ? list : list.filter(item => item.category === key);
+      key === 0 ? list : list.filter(item => item.category === key);
   };
 
   @action
