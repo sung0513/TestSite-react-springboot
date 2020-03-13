7059999f1af340159717c631dba625c70cc1daed
GiHwan123 <lgh95m@naver.com>
2020-03-13 16:44:28 +0900

    myinfo update

--- src/main/script/src/components/Myinfo.jsx
+++ src/main/script/src/components/Myinfo.jsx
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
-import { Form, Button, Container } from "react-bootstrap";
+import { Form, Button, Container, Table } from "react-bootstrap";
 class Myinfo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode: 1
+    };
+  }
   render() {
+    const { mode } = this.state;
     return (
+      <>
+      {mode === 1 ? (
       <Container>
         <Form>
           <Form.Group controlId="formBasicEmail">
@@ -19,11 +28,38 @@ class Myinfo extends Component {
           </Form.Group>
         </Form>
 
-        <Button variant="primary" type="submit">
+        <Button variant="primary" type="submit" onClick={() => this.setState({ mode: mode === 1 ? 2 : 1 })}>
           로그인
         </Button>
         <Button variant="secondary">취소</Button>
       </Container>
+      ) : ( 
+        <Table striped bordered hover variant="dark">
+          <tbody>
+          <tr>
+            <th>아이디</th>
+            <td>ㅇㅇ</td>
+          </tr>
+          <tr>
+            <th>이메일</th>
+            <td>ㅇㅇ</td>
+          </tr>
+          <tr>
+            <th>프로필</th>
+            <td>ㅇㅇ</td>
+          </tr>
+          <tr>
+            <th>이름</th>
+            <td>ㅇㅇ</td>
+          </tr>
+          <tr>
+            <th>주소</th>
+            <td>ㅇㅇ</td>
+          </tr>
+        </tbody>
+      </Table>
+      )}
+      </>
     );
   }
 }
--- src/main/script/src/components/second/Storelist.jsx
+++ src/main/script/src/components/second/Storelist.jsx
@@ -35,6 +35,7 @@ class Storelist extends Component {
               </Link>
             </Card>
           ))}
+          
         </Row>
       </Container>
     );
